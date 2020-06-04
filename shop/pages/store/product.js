081fef24b4d8e1386bf9883d1ddd26883cb9aaf9
wangchangkang <15063517240@139.com>
2020-06-04 21:01:52 +0800

    .

--- shop/pages/store/product.js
+++ shop/pages/store/product.js
@@ -6,7 +6,7 @@ Page({
 
   onLoad: function (options) {
     app.request({
-      url:'http://127.0.0.1:8000/shop/store/asdasd/products',
+      url:'http://127.0.0.1:8000/shop/store/5ed3a766f5b0e02c916ef6fa/products',
     }).then((res)=>{
       console.log(res)
       this.setData({
--- web/shop/src/main/java/work/onss/controller/StoreController.java
+++ web/shop/src/main/java/work/onss/controller/StoreController.java
@@ -3,16 +3,19 @@ package work.onss.controller;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.geo.Point;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import work.onss.domain.Product;
 import work.onss.domain.Store;
-import work.onss.exception.ServiceException;
 import work.onss.service.StoreService;
 import work.onss.vo.Work;
 
@@ -44,33 +47,15 @@ public class StoreController {
      * @param pageable 分页参数
      * @return 店铺分页
      */
-    @GetMapping(path = "store/{x}-{y}")
-    public Work<Page<Store>> store(@PathVariable(name = "x") Double x, @PathVariable(name = "y") Double y, @PageableDefault(sort = {"insertTime", "updateTime"}, direction = Sort.Direction.DESC) Pageable pageable) {
-        Page<Store> page = storeService.store(x, y, pageable);
-        return Work.success("加载成功", page);
+    @GetMapping(path = "store/{x}-{y}/near")
+    public Work<Page<Store>> store(@PathVariable(name = "x") Double x, @PathVariable(name = "y") Double y,@RequestParam(defaultValue = "100") Double r, @PageableDefault Pageable pageable) {
+        Point point = new Point(x, y);
+        Query query = Query.query(Criteria.where("point").near(point).maxDistance(r)).with(pageable);
+        List<Store> stores = mongoTemplate.find(query, Store.class);
+        Page<Store> page= new PageImpl<>(stores);
+        return Work.success(null, page);
     }
 
-    /**
-     * @param x        经度
-     * @param y        纬度
-     * @param pageable 分页参数
-     * @return 店铺分页
-     */
-    @GetMapping(path = "store/{x}-{y}/{type}")
-    public Work<Page<Store>> store(@RequestParam(name = "x") Double x, @RequestParam(name = "y") Double y, @PathVariable Integer type, @PageableDefault(sort = {"insertTime", "updateTime"}, direction = Sort.Direction.DESC) Pageable pageable) {
-        Page<Store> page = storeService.store(x, y, type, pageable);
-        return Work.success("加载成功", page);
-    }
-
-    /**
-     * @param store 店铺信息
-     * @return 店铺信息
-     */
-    @PostMapping(value = {"store"})
-    public Work<Store> store(@RequestBody Store store) throws ServiceException {
-        storeService.store(store);
-        return Work.success("申请成功,请等待客服审核", store);
-    }
 
     /**
      * @param id 主键
