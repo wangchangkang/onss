6890cd75e85070a567294e235f2c4b3664d72e79
wangchanghao <3166944856@qq.com>
2020-06-11 20:26:10 +0800

    .

--- shop/pages/store/product.js
+++ shop/pages/store/product.js
@@ -4,8 +4,9 @@ Page({
     prefix: 'http://127.0.0.1/',
   },
   onLoad: function (options) {
+    console.log(options)
     app.request({
-      url: 'http://127.0.0.1:8000/shop/store/5ed3a766f5b0e02c916ef6fa/products',
+      url: `http://127.0.0.1:8000/shop/store/${options.sid}/products`,
     }).then((res) => {
       console.log(res)
       this.setData({
@@ -15,7 +16,7 @@ Page({
   },
   onPullDownRefresh: function () {
     app.request({
-      url: 'http://127.0.0.1:8000/shop/store/5ed3a766f5b0e02c916ef6fa/products',
+      url: `http://127.0.0.1:8000/shop/store/${this.data.store.id}/products`,
     }).then((res) => {
       console.log(res)
       this.setData({
@@ -29,7 +30,7 @@ Page({
       console.log(this.data.pagination)
     } else {
       app.request({
-        url: `http://127.0.0.1:8000/shop/store/5ed3a766f5b0e02c916ef6fa/products?page=${this.data.pagination.number + 1}`,
+        url: `http://127.0.0.1:8000/shop/store/${this.data.store.id}/products?page=${this.data.pagination.number + 1}`,
       }).then((res) => {
         console.log(res)
         this.setData({
