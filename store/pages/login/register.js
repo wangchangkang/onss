dc983c0b6fd722b05a8ddd360bf737d31ac94957
wangchanghao <3166944856@qq.com>
2020-06-29 21:50:01 +0800

    .

--- store/pages/login/register.js
+++ store/pages/login/register.js
@@ -1,55 +1,65 @@
 let appInstance = getApp();
-const { windowWidth, domain, prefix, types, bankes, qualification } = appInstance.globalData;
+const { domain, banks, qualification } = appInstance.globalData;
+console.log(qualification)
+console.log(banks)
 
 Page({
   data: {
-    prefix,
-    bankes,
+    banks,
     qualification,
-    trademark:'picture/logo2.png',
-    license: {
-      name: '茌平壹玖柒柒软件有限公司',
-      number: '91371523MA3PU9M466',
-      title: '茌平壹玖柒柒',
-      type: 'ENTERPRISE',
-      industry: 23,
-      // picture: 'picture/91371523MA3PU9M466/d2d52a61266b38187993a3b8971a91dc.png'
+
+    contactName: '王先生',
+    contactIdNumber: '371523199201251250',
+    mobilePhone: '15063517240',
+    contactEmail: 'for_ccc@qq.com',
+
+    subjectType: 'SUBJECT_TYPE_ENTERPRISE',
+    licenseCopy: 'http://127.0.0.1/picture/91371523MA3PU9M466/d2d52a61266b38187993a3b8971a91dc.png',
+    licenseNumber: '91371523MA3PU9M466',
+    merchantName: '茌平壹玖柒柒软件有限公司',
+    legalPerson: '王先生',
+
+    idCardCopy: 'http://127.0.0.1/picture/91371523MA3PU9M466/d2d52a61266b38187993a3b8971a91dc.png',
+    idCardNumber: '371523199201251250',
+    idCardName: '王先生',
+    idCardNational: 'http://127.0.0.1/picture/91371523MA3PU9M466/d2d52a61266b38187993a3b8971a91dc.png',
+    cardPeriodBegin: '2013-05-02',
+    cardPeriodEnd: '2023-05-02',
+    owner: true,
+
+    uboInfo: {
+      idCardCopy: 'http://127.0.0.1/picture/91371523MA3PU9M466/d2d52a61266b38187993a3b8971a91dc.png',
+      idNumber: '371523199201251250',
+      name: '王先生',
+      idCardNational: 'http://127.0.0.1/picture/91371523MA3PU9M466/d2d52a61266b38187993a3b8971a91dc.png',
+      idPeriodBegin: '2013-05-02',
+      idPeriodEnd: '2023-05-02'
     },
-    bank: {
-      type: 'CORPORATE',
-      title: 17,
-      number: '2830051304205000010114',
-      name: '山东茌平农村商业银行胡屯支行',
-      address: {
-        code: ["110000", "110100", "110101"],
-        postcode: "100010",
-        value: ["山东省", "聊城市", "茌平县"]
-      }
-    },
-    legal: {
-      name: '王长浩',
-      idCard: '371523199201251250',
-      // idCardA: 'picture/91371523MA3PU9M466/d2d52a61266b38187993a3b8971a91dc.png',
-      // idCardB: 'picture/91371523MA3PU9M466/d2d52a61266b38187993a3b8971a91dc.pn',
-      start: '2013-05-02',
-      end: '2023-05-02'
-    },
-    qualifications: [
-      // 'picture/91371523MA3PU9M466/d2d52a61266b38187993a3b8971a91dc.png',
-      // 'picture/91371523MA3PU9M466/d2d52a61266b38187993a3b8971a91dc.png'
-    ],
+
+    merchantShortname: '茌平壹玖柒柒',
+    servicePhone: '15063517240',
     miniProgramPics: [
-      // 'picture/91371523MA3PU9M466/d2d52a61266b38187993a3b8971a91dc.png'
+      'http://127.0.0.1/picture/91371523MA3PU9M466/d2d52a61266b38187993a3b8971a91dc.png'
     ],
-    contacts: [
-      {
-        name: '王长浩',
-        idCard: '371523199201251250',
-        phone: '15063517240',
-        email: 'for_ccc@qq.com',
-      }
+
+    bankAccountType: '',
+
+    settlementId: '',
+    qualificationType: '1',
+    qualifications: [
+      'http://127.0.0.1/picture/91371523MA3PU9M466/d2d52a61266b38187993a3b8971a91dc.png'
     ],
-    focus: { license: { number: false } }
+
+    bankAccountType: 'BANK_ACCOUNT_TYPE_CORPORATE',
+    accountName: '茌平壹玖柒柒软件有限公司',
+    accountBank: 11,
+    bankAddress: {
+      code: ["110000", "110100", "110101"],
+      postcode: "100010",
+      value: ["山东省", "聊城市", "茌平县"]
+    },
+    accountNumber: '2830051304205000010114',
+    bankName: '山东茌平农村商业银行胡屯支行',
   },
 
   bindInput: function (e) {
