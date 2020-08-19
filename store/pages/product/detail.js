8676ebd9f7f1a3051e6bd390615a3aaa862a82e5
wangchanghao <3166944856@qq.com>
2020-08-19 21:48:59 +0800

    .

--- store/pages/product/detail.js
+++ store/pages/product/detail.js
@@ -9,11 +9,54 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    appInstance.request({ url: `${domain}/product/${options.id}`, method: "GET" }).then(({ content }) => {
-      this.setData({
-        index: options.index,
-        product: content
-      })
+    const customer = wx.getStorageSync('customer');
+    const authorization = wx.getStorageSync('authorization');
+    wx.request({
+      url: `${domain}/products/${options.id}?sid=${customer.store.id}`,
+      method: "GET",
+      header: {
+        authorization,
+      },
+      success: ({ data }) => {
+        const { code, msg, content } = data;
+        console.log(data)
+        switch (code) {
+          case 'success':
+            wx.showToast({
+              title: msg,
+              icon: 'success',
+              duration: 2000,
+              success: (res) => {
+                this.setData({
+                  index: options.index,
+                  product: content
+                })
+              }
+            })
+            break;
+          case 'fail.login':
+            wx.redirectTo({
+              url: '/pages/login/login',
+            })
+            break;
+          default:
+            wx.showModal({
+              title: '警告',
+              content: msg,
+              confirmColor: '#e64340',
+              showCancel: false,
+            })
+            break;
+        }
+      },
+      fail: (res) => {
+        wx.showModal({
+          title: '警告',
+          content: '加载失败',
+          confirmColor: '#e64340',
+          showCancel: false,
+        })
+      },
     })
   },
 })
