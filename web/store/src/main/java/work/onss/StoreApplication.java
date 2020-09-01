d12415f2fe8ed557998eea04ce72e46f7d71d9a6
wangchanghao <3166944856@qq.com>
2020-09-01 09:09:17 +0800

    .

--- web/store/src/main/java/work/onss/StoreApplication.java
+++ web/store/src/main/java/work/onss/StoreApplication.java
@@ -3,11 +3,13 @@ package work.onss;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @author wangchanghao
  */
 @Log4j2
+@EnableAsync
 @SpringBootApplication
 public class StoreApplication {
 
