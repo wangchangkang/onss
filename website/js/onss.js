a645e5e71c2581d8b76e723aba7a73b2a2eb660e
wangchangkang <15063517240@139.com>
2021-01-20 21:31:07 +0800

    .

---
+++ website/js/onss.js
@@ -0,0 +1,25 @@
+let h = document.documentElement.clientHeight;
+$("body").css("height", h);
+
+function getData({type = 'GET', url, data = {}, dataType = 'json'}) {
+    let $loadingToast = $('#loadingToast');
+    $loadingToast.fadeIn(1000);
+    return new Promise(function (resolve, reject) {
+        $.ajax({
+            url,
+            data,
+            type,
+            dataType,
+            success: function (res) {
+                resolve(res);
+            },
+            error: function (err) {
+                reject(err);
+            },
+            complete: function (res) {
+                $loadingToast.fadeOut(1000);
+            }
+        });
+    });
+}
+export { getData }
